8ddbde4c439b748a21542b17c51b9e0d8569d72c
Town1201 <town1201@163.com>
2017-03-22 18:41:00 +0800

    cookie test

---
+++ cookie/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+	setcookie('name','lz');
+	setcookie('name','hqq');
+	// header('Location:a.php');
+	 ?>
+	<script>
+		alert(document.cookie);
+	</script>
+</body>
+</html>
